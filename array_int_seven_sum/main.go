3f5e43186ff5e000c1fab79f46346a23c08e44da
Bruce Downs <bruce.downs@jivesoftware.com>
2017-05-26 15:27:10 -0700

    count seven sums in int array

---
+++ array_int_seven_sum/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//
+	// run via
+	// cat test.txt | go run main.go
+	//
+
+	// process each line from stdin
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("line: %s\n", line)
+
+		// tokenize input into slice of ints
+		is := make([]int, 0)
+		for _, v := range strings.Fields(line) {
+			i, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
+			}
+
+			is = append(is, i)
+		}
+
+		// hold map of int to its occurance
+		m := make(map[int]int)
+		for _, v := range is {
+			// insert or update occurance
+			if i, ok := m[v]; ok {
+				m[v] = i + 1
+			} else {
+				m[v] = 1
+			}
+
+			// check for seven partner
+			if p, ok := m[7-v]; ok {
+				// found partner
+				// print line per partner occurance
+				for i := 0; i < p; i++ {
+					fmt.Printf("%d + %d = 7\n", v, 7-v)
+				}
+			}
+		}
+
+		fmt.Println()
+	}
+}
