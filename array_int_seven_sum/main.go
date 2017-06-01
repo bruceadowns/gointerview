49369a73b5582be092fdf9e060ec5dabb039fb20
Bruce Downs <bruce.downs@jivesoftware.com>
2017-05-31 23:04:43 -0500

    Add once option

--- array_int_seven_sum/main.go
+++ array_int_seven_sum/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -12,7 +13,12 @@ func main() {
 	//
 	// run via
 	// cat test.txt | go run main.go
+	// cat test.txt | go run main.go -once
 	//
+
+	var once bool
+	flag.BoolVar(&once, "once", false, "one occurrence allowed")
+	flag.Parse()
 
 	// process each line from stdin
 	scanner := bufio.NewScanner(os.Stdin)
@@ -31,26 +37,31 @@ func main() {
 			is = append(is, i)
 		}
 
-		// hold map of int to its occurence
+		// hold map of int to its occurrence
 		m := make(map[int]int)
 		for _, v := range is {
-			// insert or update occurence
+			// insert or update occurrence
 			if i, ok := m[v]; ok {
 				m[v] = i + 1
 			} else {
 				m[v] = 1
 			}
 
+			partner := 7 - v
 			// check for seven partner
-			if p, ok := m[7-v]; ok {
-				// found partner
-				// print line per partner occurence
-				for i := 0; i < p; i++ {
-					fmt.Printf("%d + %d = 7\n", v, 7-v)
+			if p, ok := m[partner]; ok && p > 0 {
+				// found valid partner
+				if once {
+					// print line of partner occurrence
+					fmt.Printf("%d + %d = %d\n", v, partner, 7)
+					m[partner] = p - 1
+				} else {
+					// print line per partner occurrence
+					for i := 0; i < p; i++ {
+						fmt.Printf("%d + %d = %d\n", v, partner, 7)
+					}
 				}
 			}
 		}
-
-		fmt.Println()
 	}
 }
